ab78b0c06c29007edf0a4d4f3168ec897ff65e70
amir.hussain <amir.hussain@mobigesture.com>
2018-10-17 15:36:46 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics = ['Angular', 'React', 'Vue'];
+  topicHasError = true;
+  submitted =  false;
+  errorMsg = '';
+
+  userModel = new User('Rob', 'rob@test.com', 7772277772, 'default', 'morning', true);
+
+  constructor(private _enrollmentService: EnrollmentService) {}
+
+  validateTopic(value) {
+    if(value === 'default') {
+      this.topicHasError = true;
+    }else{
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this._enrollmentService.enroll(this.userModel)
+      .subscribe(
+        data => console.log('Success!',data),
+        error => this.errorMsg = error.statusText
+      )
+  }
+}
